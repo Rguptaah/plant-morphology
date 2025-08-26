4eedc32f2d394bb689b11cfb55d63a070f451a07
Rguptaah <sidarya321@gmail.com>
2025-08-26 17:24:28 +0530

    starter template

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
